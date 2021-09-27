ef71c41037fa236cf0accc1bab471f17a5fcc2f0
garvitmodi885 <garvitjain8855@gmail.com>
2021-09-27 20:30:07 +0530

    ujjwal bhiya add

---
+++ ujjawal/nodejs/loop1.js
@@ -0,0 +1,43 @@
+-------------- Loop in Js ----------------
+/*
+    1. for()
+
+    2. while()
+
+    3. do.....while()
+
+    4. forEach()
+
+*/
+
+// for(var a = 1; a <= 10; a++)
+// {
+//     console.log(a*5);
+// }
+
+// for(var a = 1; a <= 50; a++)
+// {
+//     var x = a*2;
+//     var y = x-1;
+//     console.log(y);
+// }
+//  init; comp/cond; inc/dec
+
+// var total = 0;
+// for(var a=1; a<=100; a++)
+// {
+//    total += a;
+// }
+// console.log(total);
+
+
+var a = 10;
+var ans = 1;
+for(var x = a; x>=1; x--)
+{
+    ans *= x;
+}
+console.log(ans);
+
+
+
